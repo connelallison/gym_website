2c14aefdceb6aa485097c0d1a77045a8365b5bf8
Connel Allison <connelallison@hotmail.com>
2018-12-07 11:37:27 +0000

    partially added seeds

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+require_relative('../models/lesson.rb')
+require_relative('../models/member.rb')
+require_relative('../models/member_lesson.rb')
+
+require('pry')
+
+Member.delete_all()
+Lesson.delete_all()
+MemberLesson.delete_all()
+
+
+
+# binding.pry
+
+nil
