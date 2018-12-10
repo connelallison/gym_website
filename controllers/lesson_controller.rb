6cf1b20091dbf61cacb6622887b139eceb2bb69f
Connel Allison <connelallison@hotmail.com>
2018-12-10 16:01:40 +0000

    updated delete routes to redirect to index in case of double submission. moved all gym routes to /gym/

--- controllers/lesson_controller.rb
+++ controllers/lesson_controller.rb
@@ -2,59 +2,60 @@ require_relative ("../models/member.rb")
 require_relative ("../models/lesson.rb")
 require_relative ("../models/member_lesson.rb")
 
-get '/lessons' do
+get '/gym/lessons' do
   @lessons = Lesson.all_ascending_id()
   @show_lessons = "show_lessons"
   erb(:"lessons/index")
 end
 
-get '/lessons/new' do
+get '/gym/lessons/new' do
   erb(:"lessons/new")
 end
 
-post '/lessons/:id/delete' do
+post '/gym/lessons/:id/delete' do
   @lesson = Lesson.find(params[:id].to_i())
-  @lesson.delete()
+  if (@lesson == nil); redirect('/gym/lessons'); end
+  if (@lesson!= nil); @lesson.delete(); end
   erb(:"lessons/delete")
 end
 
-get '/lessons/:id/edit' do
+get '/gym/lessons/:id/edit' do
   @lesson = Lesson.find(params[:id].to_i())
   erb(:"lessons/edit")
 end
 
-post '/lessons/:id/remove' do
+post '/gym/lessons/:id/remove' do
   @member = Member.find(params[:member_id].to_i())
   @lesson = Lesson.find(params[:lesson_id].to_i())
   @show_removed = "show_removed"
   MemberLesson.delete_by_member_and_lesson((params[:member_id].to_i()), (params[:lesson_id].to_i()))
-  redirect("/lessons/#{@lesson.id}?show_removed=true&member=#{@member.name}")
+  redirect("/gym/lessons/#{@lesson.id}?show_removed=true&member=#{@member.name}")
 end
 
-post '/lessons/:id/add' do
+post '/gym/lessons/:id/add' do
   @member = Member.find(params[:member_id].to_i())
   @lesson = Lesson.find(params[:lesson_id].to_i())
   @member_lesson = @lesson.add_member(@member)
   @member_lesson.save()
   @members = Member.all()
   @show_added = "show_added"
-  redirect("/lessons/#{@lesson.id}?show_added=true&member=#{@member.name}")
+  redirect("/gym/lessons/#{@lesson.id}?show_added=true&member=#{@member.name}")
 end
 
-post '/lessons/:id' do
+post '/gym/lessons/:id' do
   @lesson = Lesson.new(params)
   @lesson.update()
   MemberLesson.delete_ineligible()
   erb(:"lessons/update")
 end
 
-post '/lessons' do
+post '/gym/lessons' do
   @lesson = Lesson.new(params)
   @lesson.save()
   erb(:"lessons/create")
 end
 
-get '/lessons/:id' do
+get '/gym/lessons/:id' do
   @lesson = Lesson.find(params[:id].to_i())
   @show_added = "show_added" if (params[:show_added] == "true")
   @show_removed = "show_removed" if (params[:show_removed] == "true")
--- controllers/member_controller.rb
+++ controllers/member_controller.rb
@@ -2,46 +2,47 @@ require_relative ("../models/member.rb")
 require_relative ("../models/lesson.rb")
 require_relative ("../models/member_lesson.rb")
 
-get '/members' do
+get '/gym/members' do
   @members = Member.all_ascending_id()
   @show_members = "show_members"
   erb(:"members/index")
 end
 
-get '/members/new' do
+get '/gym/members/new' do
   erb(:"members/new")
 end
 
-post '/members/:id/delete' do
+post '/gym/members/:id/delete' do
   @member = Member.find(params[:id].to_i())
-  @member.delete()
+  if (@member == nil); redirect('/gym/members'); end
+  if (@member!= nil); @member.delete(); end
   erb(:"members/delete")
 end
 
-get '/members/:id/edit' do
+get '/gym/members/:id/edit' do
   @member = Member.find(params[:id].to_i())
   erb(:"members/edit")
 end
 
-post '/members/:id/remove' do
+post '/gym/members/:id/remove' do
   @member = Member.find(params[:member_id].to_i())
   @lesson = Lesson.find(params[:lesson_id].to_i())
   @show_removed = "show_removed"
   MemberLesson.delete_by_member_and_lesson((params[:member_id].to_i()), (params[:lesson_id].to_i()))
-  redirect("/members/#{@member.id}?show_removed=true&lesson=#{@lesson.course}")
+  redirect("/gym/members/#{@member.id}?show_removed=true&lesson=#{@lesson.course}")
 end
 
-post '/members/:id/add' do
+post '/gym/members/:id/add' do
   @member = Member.find(params[:member_id].to_i())
   @lesson = Lesson.find(params[:lesson_id].to_i())
   @member_lesson = @member.add_lesson(@lesson)
   @member_lesson.save()
   @lessons = Lesson.all()
   @show_added = "show_added"
-  redirect("/members/#{@member.id}?show_added=true&lesson=#{@lesson.course}")
+  redirect("/gym/members/#{@member.id}?show_added=true&lesson=#{@lesson.course}")
 end
 
-post '/members/:id' do
+post '/gym/members/:id' do
   @member = Member.new(params)
   @member.update()
   MemberLesson.delete_ineligible()
@@ -49,13 +50,13 @@ post '/members/:id' do
 end
 
 
-post '/members' do
+post '/gym/members' do
   @member = Member.new(params)
   @member.save()
   erb(:"members/create")
 end
 
-get '/members/:id' do
+get '/gym/members/:id' do
   @member = Member.find(params[:id].to_i())
   @show_added = "show_added" if (params[:show_added] == "true")
   @show_removed = "show_removed" if (params[:show_removed] == "true")
